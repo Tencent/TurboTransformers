fd197a1e21b17f81837fe1e710430f7c3423afb0
jiaruifang <jiaruifang@tencent.com>
2019-10-30 09:06:04 +0800

    add OpenMP support

--- fast_transformers/ops/cpu_layernorm_op.h
+++ fast_transformers/ops/cpu_layernorm_op.h
@@ -9,18 +9,18 @@ namespace ops {
 /***
  * Trans from (B, S, N, H) -> (B, N, S, H)
  * **/
-template <class T>
+template <typename T>
 void cpu_transpose_for_score_long(const T* a, const std::vector<unsigned>& shape, T* b);
 
-template <class T>
+template <typename T>
 void cpu_add_bias_transpose_for_score_long(const T* a,
     const T* bias, const std::vector<unsigned>& shape, T* b);
 
 
-template <class T>
+template <typename T>
 void cpu_transpose_for_score_short(const T* a, const std::vector<unsigned>& shape, T* b);
 
-template <class T>
+template <typename T>
 void cpu_add_bias(const T* bias,
     T* data,
     const int M,
