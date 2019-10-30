39bd856f9d9f153d41836297de4458c251beb0ae
Jiarui Fang <jiaruifang@tencent.com>
2019-10-30 13:17:53 +0800

    Polish tensor_test

--- fast_transformers/core/tensor_test.cpp
+++ fast_transformers/core/tensor_test.cpp
@@ -4,17 +4,19 @@
 
 TEST_CASE("TensorTest", "[tensor_allocate]") {
   REQUIRE_THROWS(FT_THROW("Test"));
-  fast_transformers::core::Tensor test_tensor(fast_transformers::core::details::CreateDLPackTensor<float, ::fast_transformers::DeviceType::CPU>({3,4}));
-  float* buff = test_tensor.mutableData<float>();
-  for(int i = 0; i < 12; ++i)
+  fast_transformers::core::Tensor test_tensor(
+      fast_transformers::core::NewDLPackTensorT<float>({3, 4}));
+  float *buff = test_tensor.mutableData<float>();
+  for (int i = 0; i < 12; ++i)
     buff[i] = i * 0.1;
-  for(int i = 0; i < 12; ++i)
-    REQUIRE(fabs(test_tensor.data<float>()[i] - i*0.1) < 1e-6);
+  for (int i = 0; i < 12; ++i)
+    REQUIRE(fabs(test_tensor.data<float>()[i] - i * 0.1) < 1e-6);
 }
 
 TEST_CASE("TensorTest2", "[tensor_init]") {
-  fast_transformers::core::Tensor test_tensor(fast_transformers::core::details::CreateDLPackTensor<float, ::fast_transformers::DeviceType::CPU>({3,4}));
+  fast_transformers::core::Tensor test_tensor(
+      fast_transformers::core::NewDLPackTensorT<float>({3, 4}));
   test_tensor.Print<float>(std::cout);
   REQUIRE(test_tensor.n_dim() == 2);
-  REQUIRE(test_tensor.numel() == 3*4);
+  REQUIRE(test_tensor.numel() == 3 * 4);
 }
