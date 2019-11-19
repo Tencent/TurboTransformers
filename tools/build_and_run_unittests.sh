fd49107fbad967434dad2d348fd3632e8508d66b
Jiarui Fang <jiaruifang@tencent.com>
2019-11-19 11:26:44 +0800

    Stablize tests

--- tools/build_and_run_unittests.sh
+++ tools/build_and_run_unittests.sh
@@ -6,4 +6,4 @@ cd /tmp/build
 cmake -DCMAKE_BUILD_TYPE=Release ${SRC_ROOT}
 make VERBOSE=1 -j $(nproc)
 pip3 install -r ${SRC_ROOT}/test_requirements.txt
-ctest -j $(nproc) --output-on-failure
+ctest --output-on-failure
