7b9656c0bfa4c136f75584ea266188c32b423bcd
Jiarui Fang <jiaruifang@tencent.com>
2019-11-19 14:20:59 +0800

    Format python code

--- fast_transformers/python/tests/bert_embedding_test.py
+++ fast_transformers/python/tests/bert_embedding_test.py
@@ -12,80 +12,118 @@ from transformers.modeling_bert import BertEmbeddings, BertConfig
 import fast_transformers
 from utils import convert2ft_tensor
 
+
 def create_test_bert_emb(batch_size: int, seq_length: int):
     class TestBertEmbedding(unittest.TestCase):
         def setUp(self) -> None:
             torch.set_grad_enabled(False)
             self.tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-            cfg = BertConfig(vocab_size_or_config_json_file=self.tokenizer.vocab_size)
+            cfg = BertConfig(
+                vocab_size_or_config_json_file=self.tokenizer.vocab_size)
             self.torch_embedding = BertEmbeddings(cfg)
-            params = {k: convert2ft_tensor(v) for k, v in
-                      self.torch_embedding.named_parameters()}
+            params = {
+                k: convert2ft_tensor(v)
+                for k, v in self.torch_embedding.named_parameters()
+            }
             self.torch_embedding.eval()
-            torch.onnx.export(self.torch_embedding, (
-                torch.ones(size=(batch_size, seq_length), dtype=torch.long),
-                torch.ones(size=(batch_size, seq_length), dtype=torch.long),
-                torch.ones(size=(batch_size, seq_length), dtype=torch.long)), f="bert-emb.onnx",
-                              output_names=['emb'])
+            torch.onnx.export(
+                self.torch_embedding,
+                (torch.ones(size=(batch_size, seq_length), dtype=torch.long),
+                 torch.ones(size=(batch_size, seq_length), dtype=torch.long),
+                 torch.ones(size=(batch_size, seq_length), dtype=torch.long)),
+                f="bert-emb.onnx",
+                output_names=['emb'])
 
             if not backend.supports_device('MKL-DNN'):
-                self.onnx_embedding = backend.prepare("bert-emb.onnx", device='CPU')
+                self.onnx_embedding = backend.prepare("bert-emb.onnx",
+                                                      device='CPU')
             else:
-                self.onnx_embedding = backend.prepare("bert-emb.onnx", device='MKL-DNN')
+                self.onnx_embedding = backend.prepare("bert-emb.onnx",
+                                                      device='MKL-DNN')
             backend.prepare(self.onnx_embedding, "CPU-MKL-DNN")
 
             self.torch_script_embedding = torch.jit.trace(
-                self.torch_embedding, (torch.ones(size=(batch_size, seq_length), dtype=torch.long),
-                                       torch.ones(size=(batch_size, seq_length), dtype=torch.long),
-                                       torch.ones(size=(batch_size, seq_length), dtype=torch.long)))
+                self.torch_embedding,
+                (torch.ones(size=(batch_size, seq_length), dtype=torch.long),
+                 torch.ones(size=(batch_size, seq_length), dtype=torch.long),
+                 torch.ones(size=(batch_size, seq_length), dtype=torch.long)))
 
-            self.ft_embedding = fast_transformers.BERTEmbedding(params['word_embeddings.weight'],
-                                                                params['position_embeddings.weight'],
-                                                                params['token_type_embeddings.weight'],
-                                                                params['LayerNorm.weight'],
-                                                                params['LayerNorm.bias'],
-                                                                cfg.hidden_dropout_prob)
+            self.ft_embedding = fast_transformers.BERTEmbedding(
+                params['word_embeddings.weight'],
+                params['position_embeddings.weight'],
+                params['token_type_embeddings.weight'],
+                params['LayerNorm.weight'], params['LayerNorm.bias'],
+                cfg.hidden_dropout_prob)
 
         def test_embedding(self):
             num_iter = 10
-            input_ids = torch.randint(low=0, high=self.tokenizer.vocab_size - 1, size=(batch_size, seq_length), dtype = torch.long)
-            position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)
+            input_ids = torch.randint(low=0,
+                                      high=self.tokenizer.vocab_size - 1,
+                                      size=(batch_size, seq_length),
+                                      dtype=torch.long)
+            position_ids = torch.arange(seq_length,
+                                        dtype=torch.long,
+                                        device=input_ids.device)
             # position_ids = position_ids.unsqueeze(0).expand_as(input_ids) #will cause bug
             position_ids = position_ids.repeat(batch_size, 1)
             token_type_ids = torch.zeros_like(input_ids, dtype=torch.long)
 
             # warming up.
             self.torch_embedding(input_ids, token_type_ids, position_ids)
-            self.torch_script_embedding(input_ids, token_type_ids, position_ids)
+            self.torch_script_embedding(input_ids, token_type_ids,
+                                        position_ids)
 
-            onnx_inputs = [input_ids.numpy(), token_type_ids.numpy(), position_ids.numpy()]
+            onnx_inputs = [
+                input_ids.numpy(),
+                token_type_ids.numpy(),
+                position_ids.numpy()
+            ]
             self.onnx_embedding.run(onnx_inputs)
             with contexttimer.Timer() as t:
                 for it in range(num_iter):
                     self.onnx_embedding.run(onnx_inputs)
-            print(f'BertEmb({batch_size}, {seq_length:03}) ONNX (with mkl-dnn) QPS {num_iter / t.elapsed}')
+            print(
+                f'BertEmb({batch_size}, {seq_length:03}) ONNX (with mkl-dnn) QPS {num_iter / t.elapsed}'
+            )
 
             with contexttimer.Timer() as t:
                 for it in range(num_iter):
-                    self.torch_embedding(input_ids, token_type_ids, position_ids)
-            print(f'BertEmb({batch_size}, {seq_length:03}) Plain PyTorch QPS {num_iter / t.elapsed}')
+                    self.torch_embedding(input_ids, token_type_ids,
+                                         position_ids)
+            print(
+                f'BertEmb({batch_size}, {seq_length:03}) Plain PyTorch QPS {num_iter / t.elapsed}'
+            )
 
             with contexttimer.Timer() as t:
                 for it in range(num_iter):
-                    self.torch_script_embedding(input_ids, token_type_ids, position_ids)
-            print(f'BertEmb({batch_size}, {seq_length:03}) TorchScript(i.e., jit) QPS {num_iter / t.elapsed}')
+                    self.torch_script_embedding(input_ids, token_type_ids,
+                                                position_ids)
+            print(
+                f'BertEmb({batch_size}, {seq_length:03}) TorchScript(i.e., jit) QPS {num_iter / t.elapsed}'
+            )
 
-            torch_result = self.torch_embedding(input_ids, token_type_ids, position_ids)
+            torch_result = self.torch_embedding(input_ids, token_type_ids,
+                                                position_ids)
             ft_result = dlpack.from_dlpack(
-                self.ft_embedding(convert2ft_tensor(input_ids), convert2ft_tensor(position_ids), convert2ft_tensor(token_type_ids)).to_dlpack())
+                self.ft_embedding(
+                    convert2ft_tensor(input_ids),
+                    convert2ft_tensor(position_ids),
+                    convert2ft_tensor(token_type_ids)).to_dlpack())
             with contexttimer.Timer() as t:
                 for it in range(num_iter):
                     ft_result = dlpack.from_dlpack(
-                        self.ft_embedding(convert2ft_tensor(input_ids), convert2ft_tensor(position_ids), convert2ft_tensor(token_type_ids)).to_dlpack())
-            self.assertTrue(torch.max(torch.abs(torch_result - ft_result)) < 1e-5)
-            print(f'BertEmb({batch_size}, {seq_length:03}) FastTransform QPS {num_iter / t.elapsed}')
+                        self.ft_embedding(
+                            convert2ft_tensor(input_ids),
+                            convert2ft_tensor(position_ids),
+                            convert2ft_tensor(token_type_ids)).to_dlpack())
+            self.assertTrue(
+                torch.max(torch.abs(torch_result - ft_result)) < 1e-5)
+            print(
+                f'BertEmb({batch_size}, {seq_length:03}) FastTransform QPS {num_iter / t.elapsed}'
+            )
 
-    globals()[f"TestBertEmbedding{batch_size}_{seq_length:03}"] = TestBertEmbedding
+    globals(
+    )[f"TestBertEmbedding{batch_size}_{seq_length:03}"] = TestBertEmbedding
 
 
 for batch_size in [1, 2]:
--- fast_transformers/python/tests/bert_output_test.py
+++ fast_transformers/python/tests/bert_output_test.py
@@ -12,76 +12,110 @@ from transformers.modeling_bert import BertConfig, BertOutput
 import fast_transformers
 from utils import convert2ft_tensor
 
+
 def create_shape_test(batch_size: int, seq_length: int):
     class TestBertOut(unittest.TestCase):
         def setUp(self) -> None:
             torch.set_grad_enabled(False)
             self.tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-            self.cfg = BertConfig(vocab_size_or_config_json_file=self.tokenizer.vocab_size)
+            self.cfg = BertConfig(
+                vocab_size_or_config_json_file=self.tokenizer.vocab_size)
             self.intermediate_size = self.cfg.intermediate_size  # 3072;
             self.hidden_size = self.cfg.hidden_size  # 768
             self.torch_bertout = BertOutput(self.cfg)
             self.torch_bertout.eval()
 
-            bertout_params = {k: v for k, v in
-                              self.torch_bertout.named_parameters()}
+            bertout_params = {
+                k: v
+                for k, v in self.torch_bertout.named_parameters()
+            }
 
             self.ft_bertout = fast_transformers.BertOutput(
                 convert2ft_tensor(bertout_params["dense.weight"]),
                 convert2ft_tensor(bertout_params["dense.bias"]),
                 convert2ft_tensor(bertout_params["LayerNorm.weight"]),
-                convert2ft_tensor(bertout_params["LayerNorm.bias"])
-            )
+                convert2ft_tensor(bertout_params["LayerNorm.bias"]))
 
-            self.intermediate_output = torch.rand(size=(batch_size, seq_length, self.intermediate_size),
-                                                  dtype=torch.float32)
-            self.attention_output = torch.rand(size=(batch_size, seq_length, self.hidden_size),
+            self.intermediate_output = torch.rand(
+                size=(batch_size, seq_length, self.intermediate_size),
+                dtype=torch.float32)
+            self.attention_output = torch.rand(size=(batch_size, seq_length,
+                                                     self.hidden_size),
                                                dtype=torch.float32)
 
             self.jit_bertout = torch.jit.trace(self.torch_bertout,
-                                               example_inputs=[self.intermediate_output, self.attention_output])
+                                               example_inputs=[
+                                                   self.intermediate_output,
+                                                   self.attention_output
+                                               ])
 
-            torch.onnx.export(self.torch_bertout, (self.intermediate_output, self.attention_output), 'bertout.onnx')
+            torch.onnx.export(
+                self.torch_bertout,
+                (self.intermediate_output, self.attention_output),
+                'bertout.onnx')
 
             if not backend.supports_device('MKL-DNN'):
-                self.onnx_bertout = backend.prepare(onnx.load('bertout.onnx'), device="CPU")
+                self.onnx_bertout = backend.prepare(onnx.load('bertout.onnx'),
+                                                    device="CPU")
                 print("Using CPU ONNX")
             else:
-                self.onnx_bertout = backend.prepare(onnx.load('bertout.onnx'), device="MKL-DNN")
+                self.onnx_bertout = backend.prepare(onnx.load('bertout.onnx'),
+                                                    device="MKL-DNN")
                 print("Using MKL-DNN ONNX")
 
         def test_bertout(self):
-            with open(f"bert_output_qps_{batch_size}_{seq_length:03}.txt", "w") as of:
+            with open(f"bert_output_qps_{batch_size}_{seq_length:03}.txt",
+                      "w") as of:
                 num_steps = 100
-                torch_result = self.torch_bertout(self.intermediate_output, self.attention_output)
+                torch_result = self.torch_bertout(self.intermediate_output,
+                                                  self.attention_output)
                 with contexttimer.Timer() as t:
                     for it in range(num_steps):
-                        torch_result = self.torch_bertout(self.intermediate_output, self.attention_output)
+                        torch_result = self.torch_bertout(
+                            self.intermediate_output, self.attention_output)
 
-                print(f"BertOut({batch_size}, {seq_length:03}) Torch QPS {num_steps / t.elapsed}", file=of)
+                print(
+                    f"BertOut({batch_size}, {seq_length:03}) Torch QPS {num_steps / t.elapsed}",
+                    file=of)
 
                 with contexttimer.Timer() as t:
                     for it in range(num_steps):
-                        self.jit_bertout(self.intermediate_output, self.attention_output)
-                print(f'BertOut({batch_size}, {seq_length:03}) Jit QPS {num_steps / t.elapsed}', file=of)
+                        self.jit_bertout(self.intermediate_output,
+                                         self.attention_output)
+                print(
+                    f'BertOut({batch_size}, {seq_length:03}) Jit QPS {num_steps / t.elapsed}',
+                    file=of)
 
-                onnx_input_feeds = [self.intermediate_output.numpy(), self.attention_output.numpy()]
+                onnx_input_feeds = [
+                    self.intermediate_output.numpy(),
+                    self.attention_output.numpy()
+                ]
                 self.onnx_bertout.run(inputs=onnx_input_feeds)
 
                 with contexttimer.Timer() as t:
                     for it in range(num_steps):
                         self.onnx_bertout.run(inputs=onnx_input_feeds)
-                print(f'BertOut({batch_size}, {seq_length:03}) ONNX QPS {num_steps / t.elapsed}', file=of)
+                print(
+                    f'BertOut({batch_size}, {seq_length:03}) ONNX QPS {num_steps / t.elapsed}',
+                    file=of)
 
                 ft_result = dlpack.from_dlpack(
-                    self.ft_bertout(convert2ft_tensor(self.intermediate_output), convert2ft_tensor(self.attention_output)).to_dlpack())
+                    self.ft_bertout(
+                        convert2ft_tensor(self.intermediate_output),
+                        convert2ft_tensor(self.attention_output)).to_dlpack())
                 with contexttimer.Timer() as t:
                     for it in range(num_steps):
                         ft_result = dlpack.from_dlpack(
-                            self.ft_bertout(convert2ft_tensor(self.intermediate_output), convert2ft_tensor(self.attention_output)).to_dlpack())
+                            self.ft_bertout(
+                                convert2ft_tensor(self.intermediate_output),
+                                convert2ft_tensor(
+                                    self.attention_output)).to_dlpack())
 
-                print(f"BertOut({batch_size}, {seq_length:03}) FastTransform QPS {num_steps / t.elapsed}", file=of)
-                self.assertTrue( torch.max(torch.abs(torch_result - ft_result)) < 1e-4 )
+                print(
+                    f"BertOut({batch_size}, {seq_length:03}) FastTransform QPS {num_steps / t.elapsed}",
+                    file=of)
+                self.assertTrue(
+                    torch.max(torch.abs(torch_result - ft_result)) < 1e-4)
 
     TestBertOut.__name__ = f"TestBertOut_BatchSize_{batch_size}_SeqLen_{seq_length}"
 
@@ -90,8 +124,10 @@ def create_shape_test(batch_size: int, seq_length: int):
     return TestBertOut
 
 
-TestCases = [create_shape_test(batch_size=batch_size, seq_length=seq_length) for seq_length in
-             (20, 40, 60, 80, 100, 120) for batch_size in (1, 2)]
+TestCases = [
+    create_shape_test(batch_size=batch_size, seq_length=seq_length)
+    for seq_length in (20, 40, 60, 80, 100, 120) for batch_size in (1, 2)
+]
 
 # TestBertOut = create_shape_test(batch_size=1, seq_length=20)
 
