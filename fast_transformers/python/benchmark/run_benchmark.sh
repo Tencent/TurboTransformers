1befb269b16890ec0b1f9c33f2ac3b6e300f861d
Jiarui Fang <jiaruifang@tencent.com>
2019-11-21 20:36:52 +0800

    Add benchmark script

---
+++ fast_transformers/python/benchmark/run_benchmark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+NUM_THREADS=(1 2 4 8)
+FRAMEWORKS=("torch" "torch_jit" "fast-transformers" "onnxruntime-cpu" "onnxruntime-mkldnn")
+SEQ_LEN=(10 20 40 60 80 120)
+BATCH_SIZE=(1 2)
+N=20
+MODEL="bert-base-chinese"
+for n_th in ${NUM_THREADS[*]}
+do
+  for batch_size in ${BATCH_SIZE[*]}
+  do
+    for seq_len in ${SEQ_LEN[*]}
+    do
+      for framework in ${FRAMEWORKS[*]}
+      do
+        python benchmark.py ${MODEL} --seq_len=${seq_len} --batch_size=${batch_size}\
+            -n ${N} --framework=${framework} --num_threads=${n_th}
+      done
+    done
+  done
+done
