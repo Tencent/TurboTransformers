c61bb2ea1c9a062e07ce4ac6856b834dbfb05edf
jiaruifang <jiaruifang@tencent.com>
2019-12-25 15:32:31 +0800

    polish gpu_activation_kernel

--- fast_transformers/layers/kernels/gpu_activation_kernel.cu
+++ fast_transformers/layers/kernels/gpu_activation_kernel.cu
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "fast_transformers/layers/kernels/gpu_activation_kernel.h"
 #include <immintrin.h>
 #include <numeric>
@@ -6,19 +22,18 @@
 namespace fast_transformers {
 namespace layers {
 namespace kernels {
-template <typename T>
+
 static __inline__ __device__
-T gelu(T x)
+float gelu(float x)
 {
   float cdf = 0.5f * (1.0f + tanhf((0.7978845608028654f * (x + 0.044715f * x * x * x))));
   return x * cdf;
 }
 
-template <typename T>
 static __global__
-void add_bias_act(T* out, const T* bias, int batch_size, int feature_dim)
+void add_bias_act(float* out, const float* bias, int batch_size, int feature_dim)
 {
-  T val, reg_bias;
+  float val, reg_bias;
 
   int row_id = blockIdx.x;
   int ite = feature_dim / blockDim.x;
@@ -31,21 +46,20 @@ void add_bias_act(T* out, const T* bias, int batch_size, int feature_dim)
 
     while(row_id < batch_size){
       val = out[tid + i * blockDim.x + row_id * feature_dim] + reg_bias;
-      out[tid + i * blockDim.x + row_id * feature_dim] = gelu<T>(val);
+      out[tid + i * blockDim.x + row_id * feature_dim] = gelu(val);
       row_id += gridDim.x;
     }
   }
 }
-
-template <typename T>
-void GPUAddBiasGeLUActKernel(const T* bias_data, T* out_data, int64_t batch_size, int64_t feature_dim, cudaStream_t stream) {
+template <>
+void GPUAddBiasGeLUActKernel(const float* bias_data, float* out_data, int64_t batch_size, int64_t feature_dim, cudaStream_t stream) {
   dim3 grid(batch_size / 4);
   dim3 block(feature_dim / 4);
-  add_bias_act<T><<<grid, block, 0, stream>>>(out_data, bias_data, batch_size, feature_dim);
+  if (feature_dim / 4 > 1024) {
+    throw std::runtime_error("GPUAddBiasGeLUActKernel thread block size large than 1024");
+  }
+  add_bias_act<<<grid, block, 0, stream>>>(out_data, bias_data, batch_size, feature_dim);
 }
-
-
-template void GPUAddBiasGeLUActKernel<float>(const float* bias_data, float* out_data, int64_t batch_size, int64_t feature_dim, cudaStream_t stream);
 
 }  // namespace kernels
 }  // namespace layers
