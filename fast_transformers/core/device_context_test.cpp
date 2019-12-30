b2fdc2e71e1a8cb6c318bfada21e7f2a650b099d
jiaruifang <jiaruifang@tencent.com>
2019-12-30 21:08:26 +0800

    add cuda allacator.
    Former-commit-id: 7dd25ad06c269be4629ee2bf169822f31d8288c7

--- fast_transformers/core/device_context_test.cpp
+++ fast_transformers/core/device_context_test.cpp
@@ -13,24 +13,6 @@ TEST_CASE("CUDADeviceContext", "init") {
   cuda_ctx.Wait();
 }
 
-TEST_CASE("allocate and free") {
-  CUDADeviceContext& cuda_ctx = CUDADeviceContext::GetInstance();
-  void* device_mem = cuda_ctx.allocate(20 * 1e9);
-  cuda_ctx.free(device_mem, 20 * 1e9);
-  cuda_ctx.Wait();
-}
-
-TEST_CASE("allocate too much") {
-  CUDADeviceContext& cuda_ctx = CUDADeviceContext::GetInstance();
-  void* device_mem1 = cuda_ctx.allocate(10 * 1e9);
-  void* device_mem2 = cuda_ctx.allocate(10 * 1e9);
-  // allocate 30 GB
-  cuda_ctx.free(device_mem1, 10 * 1e9);
-  void* device_mem3 = cuda_ctx.allocate(10 * 1e9);
-  cuda_ctx.free(device_mem2, 10 * 1e9);
-  cuda_ctx.free(device_mem3, 10 * 1e9);
-  cuda_ctx.Wait();
-}
 #endif
 
 }  // namespace core
