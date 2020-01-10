8788d594275a2c5096eacc5215bddc559fc928e4
jiaruifang <jiaruifang@tencent.com>
2020-01-10 18:05:33 +0800

    Revert "add perpare_bert_mask gpu implementations."
    This reverts commit 1f63e27e1c23dbd52e7dac43c165cff1462f7f3f.

--- fast_transformers/layers/prepare_bert_masks.cpp
+++ fast_transformers/layers/prepare_bert_masks.cpp
@@ -1,13 +1,10 @@
 #include "prepare_bert_masks.h"
+
 #include <stdint.h>
 
-#ifdef FT_WITH_CUDA
-#include "fast_transformers/layers/kernels/gpu_utils.h"
-#endif
-//#include "fast_transformers/core/eigen-tensor.h"
+#include "fast_transformers/core/eigen-tensor.h"
 namespace fast_transformers {
 namespace layers {
-
 void PrepareBertMasks::operator()(const core::Tensor& inputs,
                                   core::Tensor* att_mask,
                                   core::Tensor* seq_type,
@@ -20,11 +17,7 @@ void PrepareBertMasks::operator()(const core::Tensor& inputs,
 
     // fill range
     for (int64_t row_id = 0; row_id < inputs.shape(0); ++row_id) {
-#ifdef FT_WITH_CUDA
-      kernels::thrust_sequence(pos_ids_ptr, inputs.shape(1));
-#else
       std::iota(pos_ids_ptr, pos_ids_ptr + inputs.shape(1), 0);
-#endif
       pos_ids_ptr += inputs.shape(1);
     }
   }
@@ -33,25 +26,16 @@ void PrepareBertMasks::operator()(const core::Tensor& inputs,
     // seq_type.zeros_like(inputs)
     seq_type->Reshape<int64_t>({inputs.shape(0), inputs.shape(1)},
                                inputs.device_type(), inputs.device_id());
-#ifdef FT_WITH_CUDA
-    kernels::thrust_fill(seq_type->mutableData<int64_t>(), seq_type->numel(),
-                         int64_t(0));
-#else
+
     std::memset(seq_type->mutableData<int64_t>(), 0,
                 sizeof(int64_t) * seq_type->numel());
-#endif
   }
 
   if (att_mask->is_null()) {
     att_mask->Reshape<int64_t>({inputs.shape(0), inputs.shape(1)},
                                inputs.device_type(), inputs.device_id());
-#ifdef FT_WITH_CUDA
-    kernels::thrust_fill(att_mask->mutableData<int64_t>(), att_mask->numel(),
-                         int64_t(1));
-#else
     std::fill(att_mask->mutableData<int64_t>(),
               att_mask->mutableData<int64_t>() + att_mask->numel(), 1);
-#endif
   }
 
   // cast att_mask to float
@@ -59,16 +43,10 @@ void PrepareBertMasks::operator()(const core::Tensor& inputs,
       {att_mask->shape(0), 1, 1, att_mask->shape(1)}, inputs.device_type(),
       inputs.device_id());
 
-#ifdef FT_WITH_CUDA
-  kernels::thrust_transform(att_mask->mutableData<int64_t>(),
-                            extended_attention_mask->mutableData<float>(),
-                            att_mask->numel());
-#else
   std::transform(att_mask->data<int64_t>(),
                  att_mask->data<int64_t>() + att_mask->numel(),
                  extended_attention_mask->mutableData<float>(),
                  [](int64_t v) { return -10000.0f * (1 - v); });
-#endif
 }
 }  // namespace layers
 }  // namespace fast_transformers
