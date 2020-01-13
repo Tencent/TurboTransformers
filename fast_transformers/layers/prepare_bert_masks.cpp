a7f2ac37c3a3c10e597118fa49989d671f11607f
jiaruifang <jiaruifang@tencent.com>
2020-01-13 21:26:26 +0800

    rename thrust to gpu.

--- fast_transformers/layers/prepare_bert_masks.cpp
+++ fast_transformers/layers/prepare_bert_masks.cpp
@@ -4,7 +4,7 @@
 #ifdef FT_WITH_CUDA
 #include "fast_transformers/layers/kernels/gpu_utils.h"
 #endif
-//#include "fast_transformers/core/eigen-tensor.h"
+
 namespace fast_transformers {
 namespace layers {
 
@@ -50,6 +50,7 @@ void PrepareBertMasks::operator()(const core::Tensor& inputs,
                    extended_attention_mask->mutableData<float>(),
                    [](int64_t v) { return -10000.0f * (1 - v); });
   } else if (inputs.device_type() == kDLGPU) {
+#ifdef FT_WITH_CUDA
     if (position_ids->is_null()) {
       auto pos_ids_ptr = position_ids->Reshape<int64_t>(
           {inputs.shape(0), inputs.shape(1)}, inputs.device_type(),
@@ -57,7 +58,7 @@ void PrepareBertMasks::operator()(const core::Tensor& inputs,
 
       // fill range
       for (int64_t row_id = 0; row_id < inputs.shape(0); ++row_id) {
-        kernels::thrust_sequence(pos_ids_ptr, inputs.shape(1));
+        kernels::gpu_sequence(pos_ids_ptr, inputs.shape(1));
         pos_ids_ptr += inputs.shape(1);
       }
     }
@@ -66,24 +67,29 @@ void PrepareBertMasks::operator()(const core::Tensor& inputs,
       // seq_type.zeros_like(inputs)
       seq_type->Reshape<int64_t>({inputs.shape(0), inputs.shape(1)},
                                  inputs.device_type(), inputs.device_id());
-      kernels::thrust_fill(seq_type->mutableData<int64_t>(), seq_type->numel(),
-                           int64_t(0));
+      kernels::gpu_fill(seq_type->mutableData<int64_t>(), seq_type->numel(),
+                        int64_t(0));
     }
 
     if (att_mask->is_null()) {
       att_mask->Reshape<int64_t>({inputs.shape(0), inputs.shape(1)},
                                  inputs.device_type(), inputs.device_id());
-      kernels::thrust_fill(att_mask->mutableData<int64_t>(), att_mask->numel(),
-                           int64_t(1));
+      kernels::gpu_fill(att_mask->mutableData<int64_t>(), att_mask->numel(),
+                        int64_t(1));
     }
 
     // cast att_mask to float
     extended_attention_mask->Reshape<float>(
         {att_mask->shape(0), 1, 1, att_mask->shape(1)}, inputs.device_type(),
         inputs.device_id());
-    kernels::thrust_transform(att_mask->mutableData<int64_t>(),
-                              extended_attention_mask->mutableData<float>(),
-                              att_mask->numel());
+    kernels::gpu_transform(att_mask->mutableData<int64_t>(),
+                           extended_attention_mask->mutableData<float>(),
+                           att_mask->numel());
+#else
+    FT_THROW("The current code is not compiled with CUDA.");
+#endif
+  } else {
+    FT_THROW("device_type is not supported");
   }
 }
 
