df5d110733f57878f2c924ea173031a73d940b81
jiaruifang <jiaruifang@tencent.com>
2020-02-25 15:15:03 +0800

    add linux/cuda version in gpu docker build rm unnecessary code in gpu benchmark.

--- tools/build_docker_gpu.sh
+++ tools/build_docker_gpu.sh
@@ -2,8 +2,10 @@
 cd $(dirname $0)/../
 set -xe
 
-VERSION=$(cat CMakeLists.txt | grep FAST_TRANSFORMERS_VERSION | \
+SOTWARE_VERSION=$(cat CMakeLists.txt | grep FAST_TRANSFORMERS_VERSION | \
     sed 's#set(FAST_TRANSFORMERS_VERSION ##g' | sed 's#)##g')
+CUDA_VERSION=10.0
+LINUX_VERSION=ubuntu18.04
 
 docker build ${EXTRA_ARGS} \
-	-t ccr.ccs.tencentyun.com/mmspr/fast_transformer:${VERSION}-gpu-dev -f Dockerfile.gpu_dev  .
+	--build-arg CUDA_VERSION=${CUDA_VERSION} --build-arg LINUX_VERSION=${LINUX_VERSION} -t ccr.ccs.tencentyun.com/mmspr/fast_transformer:${SOTWARE_VERSION}-cuda${CUDA_VERSION}-gpu-dev -f Dockerfile.gpu_dev  .
