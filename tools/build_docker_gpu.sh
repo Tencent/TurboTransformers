91a298792a8f32e0e2f064b18865b1549e2ca1b2
jiaruifang <jiaruifang@tencent.com>
2020-02-25 22:34:20 +0800

    fix ENV problem in docker

---
+++ tools/build_docker_gpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $(dirname $0)/../
+set -xe
+
+SOTWARE_VERSION=$(cat CMakeLists.txt | grep FAST_TRANSFORMERS_VERSION | \
+    sed 's#set(FAST_TRANSFORMERS_VERSION ##g' | sed 's#)##g')
+CUDA_VERSION=9.1
+LINUX_VERSION=ubuntu16.04
+
+docker build ${EXTRA_ARGS} \
+	--build-arg CUDA_VERSION=${CUDA_VERSION} --build-arg LINUX_VERSION=${LINUX_VERSION} -t ccr.ccs.tencentyun.com/mmspr/fast_transformer:${SOTWARE_VERSION}-cuda${CUDA_VERSION}-gpu-dev -f Dockerfile.gpu_dev  .
