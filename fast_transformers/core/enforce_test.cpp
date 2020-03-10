7ba292dcb7b2f643093af3c38e3ef58f0fad17ca
florianzhao <florianzhao@tencent.com>
2020-03-10 13:06:27 +0800

    remove not usered code

--- fast_transformers/core/enforce_test.cpp
+++ fast_transformers/core/enforce_test.cpp
@@ -1,11 +1,11 @@
 // Copyright 2020 Tencent
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,14 +14,8 @@
 
 #define CATCH_CONFIG_MAIN
 #include "fast_transformers/core/enforce.h"
+
 #include "catch2/catch.hpp"
-// TEST(enforce, throw) { ASSERT_THROW(WXBOT_THROW("Test"),
-// wxbot::base::EnforceNotMet); } TEST(enforce, check) {
-// ASSERT_THROW(WXBOT_ENFORCE(false, "Not matched"),
-// wxbot::base::EnforceNotMet); ASSERT_THROW(WXBOT_ENFORCE_EQ(1, 2, "Not
-// matched"), wxbot::base::EnforceNotMet); ASSERT_THROW(WXBOT_ENFORCE_NE(1, 1,
-// "Matched"), wxbot::base::EnforceNotMet);
-//}
 TEST_CASE("Enforce", "[enforce]") {
   REQUIRE_THROWS(FT_THROW("Test"));
   bool ok = false;
