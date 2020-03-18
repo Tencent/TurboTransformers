482ed142d86cacd578489ae5b4b07105dc5ac64b
florianzhao <florianzhao@tencent.com>
2020-03-18 15:27:38 +0800

    remove static

--- turbo_transformers/layers/bert_attention.cpp
+++ turbo_transformers/layers/bert_attention.cpp
@@ -50,7 +50,7 @@ void BertAttention::operator()(const core::Tensor& input_tensor,
                          input_tensor.device_type(), input_tensor.device_id());
 
   // 1. temp_qkv = MatMul(input)
-  static core::Tensor temp_qkv(nullptr);
+  core::Tensor temp_qkv(nullptr);
   temp_qkv.Reshape<float>({3, batch_size, seq_length, hidden_size},
                           input_tensor.device_type(), input_tensor.device_id());
 
@@ -59,7 +59,7 @@ void BertAttention::operator()(const core::Tensor& input_tensor,
   // 2. qkv = transpose(temp_qkv + bias)
   // Since `SplitAddBiasTransposeForScore` does not support inplace,
   // qkv and temp_qkv cannot be same tensor
-  static core::Tensor qkv(nullptr);
+  core::Tensor qkv(nullptr);
   qkv.Reshape<float>(
       {3, batch_size, num_attention_heads_, seq_length, size_per_head},
       input_tensor.device_type(), input_tensor.device_id());
@@ -72,7 +72,7 @@ void BertAttention::operator()(const core::Tensor& input_tensor,
   auto v = qkv[2];
 
   // 4. att_score = softmax((q * k^T)*1/sqrt(size_per_head) + att_mask)
-  static core::Tensor att_score(nullptr);
+  core::Tensor att_score(nullptr);
   att_score.Reshape<float>(
       {batch_size, num_attention_heads_, seq_length, seq_length},
       input_tensor.device_type(), input_tensor.device_id());
@@ -84,14 +84,14 @@ void BertAttention::operator()(const core::Tensor& input_tensor,
       1 / std::sqrt(static_cast<float>(size_per_head)));
 
   // 5. ctx = v * att_score
-  static core::Tensor context_layer(nullptr);
+  core::Tensor context_layer(nullptr);
   context_layer.Reshape<float>(
       {batch_size, num_attention_heads_, seq_length, size_per_head},
       input_tensor.device_type(), input_tensor.device_id());
   kernels::BatchMatMul(att_score, false, v, false, 1.0, &context_layer, 0.0);
 
   // 6. self_att_out = transpose(ctx)
-  static core::Tensor self_attr_out(nullptr);
+  core::Tensor self_attr_out(nullptr);
   self_attr_out.Reshape<float>(
       {batch_size, seq_length, num_attention_heads_ * size_per_head},
       input_tensor.device_type(), input_tensor.device_id());
