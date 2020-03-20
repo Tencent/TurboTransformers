b5563564b7ce4ebcab03f17ab72464932bd34d72
jiaruifang <jiaruifang@tencent.com>
2020-03-20 23:58:52 +0800

    1. optimize activation kernel, make it more rubust. 2. modified pre-commit hook

--- tools/build_docker_gpu.sh
+++ tools/build_docker_gpu.sh
@@ -18,8 +18,8 @@ set -xe
 VERSION=$(cat ../CMakeLists.txt | grep turbo_transformers_VERSION | \
     sed 's#set(turbo_transformers_VERSION ##g' | sed 's#)##g')
 
-CUDA_VERSION=9.0
-DOCKER_BASE=${CUDA_VERSION}-cudnn7-devel-ubuntu16.04
+CUDA_VERSION=10.0
+DOCKER_BASE=${CUDA_VERSION}-cudnn7-devel-ubuntu18.04
 PYTORCH_VERSION=1.1.0
 sed 's#IMAGE_BASE#nvidia/cuda:'${DOCKER_BASE}'#g' ./docker/Dockerfile_dev.gpu |
 sed 's#CUDA_VERSION#'${CUDA_VERSION}'#g'         |
