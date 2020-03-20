9902bfc091c19f03385971af3815768ac6aceb75
Joseph Yu <josephyu@tencent.com>
2020-03-20 12:21:30 +0800

    Fix licence

--- turbo_transformers/loaders/modeling_bert.h
+++ turbo_transformers/loaders/modeling_bert.h
@@ -1,7 +1,3 @@
-#pragma once
-#include <functional>
-#include <memory>
-#include <vector>
 // Copyright 2020 Tencent
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#pragma once
+#include <functional>
+#include <memory>
+#include <vector>
 
 #include "dlpack/dlpack.h"
 namespace turbo_transformers {
