43f83135a4f859698d83038d772bf31663205163
jiaruifang <jiaruifang@tencent.com>
2020-04-20 15:50:39 +0800

    jiaruifang/robust_transpose (merge request !192)
    Squash merge branch 'jiaruifang/robust_transpose' into 'develop'
    
    * fuse some multiplications in transpose
    
    * make transpose more robust and easy to read.

--- turbo_transformers/layers/kernels/gpu_transpose_kernel.cu
+++ turbo_transformers/layers/kernels/gpu_transpose_kernel.cu
@@ -23,47 +23,38 @@ namespace layers {
 namespace kernels {
 
 /*
-   (batch_size, seq_len, weight_num, num_attention_heads, size_per_head) ->
-   (weight_num, batch_size, head_num, seq_len, size_per_head)
+   input : (batch_size, seq_len, weight_num, head_num, size_per_head) ->
+   output : (weight_num, batch_size, head_num, seq_len, size_per_head)
+   bias (weight_num, head_num, size_per_head)
    */
 static __global__ void split_add_bias_transpose_for_score(
-    const float* input_data, const float* bias_data, float* output_data,
-    const int batch_size, const int seq_len, const int head_num,
-    const int size_per_head, const int word_per_block) {
-  const float* data_ptr;
-  float* buf_ptr;
-  const float* bias_ptr;
+    const float* input_data, const float* bias_data, const int batch_size,
+    const int seq_len, const int head_num, const int weight_num,
+    const int size_per_head, float* output_data) {
+  int tid = threadIdx.x;
+  int bid = blockIdx.x;
+  int idx = tid;
+  int batch_id = bid / (seq_len * weight_num * head_num);
+  int seq_id =
+      bid % (seq_len * weight_num * head_num) / (weight_num * head_num);
+  int weight_id = bid % (weight_num * head_num) / head_num;
+  int head_id = bid % head_num;
 
-  int m = batch_size * seq_len;
-  int n = head_num * size_per_head;
-
-  int col_offset = blockIdx.x % 3;
-  int row_offset = blockIdx.x / 3 * word_per_block % m;
-
-  data_ptr = input_data + row_offset * 3 * n + col_offset * n;
-  buf_ptr = output_data + col_offset * m * n;
-  bias_ptr = bias_data + col_offset * n;
-
-  int head_id = threadIdx.x % n / size_per_head;
-  int hidden_id = threadIdx.x % size_per_head;
-#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ > 300
-  float bias = __ldg(&bias_ptr[threadIdx.x]);
-#else
-  float bias = bias_ptr[threadIdx.x];
-#endif
-
-  for (int i = row_offset; i < row_offset + word_per_block; ++i) {
-    if (i >= m) break;
-    int seq_id = i % seq_len;
-    int batch_id = i / seq_len;
-
-    int target_id = batch_id * (seq_len * head_num * size_per_head) +
-                    head_id * seq_len * size_per_head + seq_id * size_per_head +
-                    hidden_id;
-    float tmp = data_ptr[threadIdx.x] + bias;
-
-    buf_ptr[target_id] = tmp;
-    data_ptr += 3 * n;
+  int head_num_size_per_head = head_num * size_per_head;
+  int weight_id_head_num_size_per_head = weight_id * head_num_size_per_head;
+  int head_id_size_per_head = head_id * size_per_head;
+  while (idx < size_per_head) {
+    float bias_val = bias_data[weight_id_head_num_size_per_head +
+                               head_id_size_per_head + idx];
+    output_data[weight_id * batch_size * seq_len * head_num_size_per_head +
+                batch_id * seq_len * head_num_size_per_head +
+                head_id * +seq_id * size_per_head + idx] =
+        input_data[batch_id * seq_len * weight_num * head_num_size_per_head +
+                   seq_id * weight_num * head_num_size_per_head +
+                   weight_id_head_num_size_per_head + head_id_size_per_head +
+                   idx] +
+        bias_val;
+    idx += blockDim.x;
   }
 }
 
@@ -72,46 +63,44 @@ void GPUSplitAddBiasTransposeForScore(
     const float* input_data, const float* bias_data, float* out_data,
     int64_t batch_size, int64_t seq_len, int64_t weight_num,
     int64_t num_attention_heads, int64_t size_per_head, cudaStream_t stream) {
-  const int word_per_block = 32;
-  const int n = num_attention_heads * size_per_head;
-  const int m = batch_size * seq_len;
-  if (n > 1024) {
-    throw std::runtime_error(
-        "GPUSplitAddBiasTransposeForScore thread block size large than 1024");
-  }
-  // x, y
-  dim3 grid(3 * (m + word_per_block - 1) / word_per_block);
-  dim3 block(n);
+  const int n = size_per_head;
+  const int m = batch_size * seq_len * num_attention_heads * weight_num;
+  dim3 grid(m);
+  dim3 block(min(n, 1024));
   split_add_bias_transpose_for_score<<<grid, block, 0, stream>>>(
-      input_data, bias_data, out_data, batch_size, seq_len, num_attention_heads,
-      size_per_head, word_per_block);
+      input_data, bias_data, batch_size, seq_len, num_attention_heads,
+      weight_num, size_per_head, out_data);
 }
 
-// copyright nvidia
 static __global__ void transpose(const float* src, float* dst,
                                  const int batch_size, const int seq_len,
                                  const int head_num, const int size_per_head) {
+  int tid = threadIdx.x;
   int batch_id = blockIdx.x / (head_num * seq_len);
   int seq_id = blockIdx.x % seq_len;
   int head_id = (blockIdx.x % (head_num * seq_len)) / seq_len;
-  dst[batch_id * (head_num * seq_len * size_per_head) +
-      seq_id * head_num * size_per_head + head_id * size_per_head +
-      threadIdx.x] = src[blockIdx.x * size_per_head + threadIdx.x];
+
+  int idx = tid;
+  while (idx < size_per_head) {
+    dst[batch_id * (head_num * seq_len * size_per_head) +
+        seq_id * head_num * size_per_head + head_id * size_per_head + idx] =
+        src[blockIdx.x * size_per_head + idx];
+    idx += blockDim.x;
+  }
 }
 
+/*
+   (batch_size, seq_len, num_attention_heads, size_per_head) ->
+   (batch_size, head_num, seq_len, size_per_head)
+   */
 template <>
 void GPUTransposeForScore(const float* input_data, float* output_data,
                           int64_t batch_size, int64_t seq_len,
                           int64_t num_attention_heads, int64_t size_per_head,
                           cudaStream_t stream) {
-  const int seq_per_block = 1;
   dim3 grid, block;
-  grid.x = batch_size * num_attention_heads * seq_len / seq_per_block;
-  block.x = seq_per_block * size_per_head;
-  if (block.x > 1024) {
-    throw std::runtime_error(
-        "GPUTransposeForScore thread block size large than 1024");
-  }
+  grid.x = batch_size * num_attention_heads * seq_len;
+  block.x = min(1024, int(size_per_head));
   transpose<<<grid, block, 0, stream>>>(input_data, output_data, batch_size,
                                         seq_len, num_attention_heads,
                                         size_per_head);
--- turbo_transformers/layers/kernels/transpose_test.cpp
+++ turbo_transformers/layers/kernels/transpose_test.cpp
@@ -28,46 +28,51 @@ namespace kernels {
 #ifdef TT_WITH_CUDA
 TEST_CASE("splitaddtranspose-gpu-test") {
   const std::vector<int64_t> num_attention_heads_list{12};
-  const std::vector<int64_t> batch_size_list{1, 20, 24};
+  const std::vector<int64_t> batch_size_list{1, 12, 20};
   const std::vector<int64_t> seq_length_list{10, 20, 32, 64, 128};
 
-  for (auto num_attention_heads : num_attention_heads_list)
-    for (auto batch_size : batch_size_list)
-      for (auto seq_length : seq_length_list) {
-        std::cout << num_attention_heads << ", " << batch_size << " ,"
-                  << seq_length << std::endl;
-        core::Tensor input_tensor_cpu(nullptr), input_tensor_gpu(nullptr);
-        std::tie(input_tensor_cpu, input_tensor_gpu) =
-            common::CreateAndFillRandomForCPUGPUTensors<float>(
-                {batch_size, seq_length, 3, num_attention_heads, 64});
+  for (auto hidden_size : {64, 2000})
+    for (auto num_attention_heads : num_attention_heads_list)
+      for (auto batch_size : batch_size_list)
+        for (auto seq_length : seq_length_list) {
+          std::cout << num_attention_heads << ", " << batch_size << " ,"
+                    << seq_length << std::endl;
+          core::Tensor input_tensor_cpu(nullptr), input_tensor_gpu(nullptr);
+          std::tie(input_tensor_cpu, input_tensor_gpu) =
+              common::CreateAndFillRandomForCPUGPUTensors<float>(
+                  {batch_size, seq_length, 3, num_attention_heads,
+                   hidden_size});
 
-        core::Tensor bias_tensor_cpu(nullptr), bias_tensor_gpu(nullptr);
-        std::tie(bias_tensor_cpu, bias_tensor_gpu) =
-            common::CreateAndFillRandomForCPUGPUTensors<float>(
-                {3, num_attention_heads, 64});
+          core::Tensor bias_tensor_cpu(nullptr), bias_tensor_gpu(nullptr);
+          std::tie(bias_tensor_cpu, bias_tensor_gpu) =
+              common::CreateAndFillRandomForCPUGPUTensors<float>(
+                  {3, num_attention_heads, hidden_size});
 
-        turbo_transformers::core::Tensor output_tensor_gpu(
-            turbo_transformers::core::NewDLPackTensorT<float>(
-                {3, batch_size, num_attention_heads, seq_length, 64}, kDLGPU,
-                0));
-        turbo_transformers::core::Tensor output_tensor_cpu(
-            turbo_transformers::core::NewDLPackTensorT<float>(
-                {3, batch_size, num_attention_heads, seq_length, 64}, kDLCPU,
-                0));
+          turbo_transformers::core::Tensor output_tensor_gpu(
+              turbo_transformers::core::NewDLPackTensorT<float>(
+                  {3, batch_size, num_attention_heads, seq_length, hidden_size},
+                  kDLGPU, 0));
+          turbo_transformers::core::Tensor output_tensor_cpu(
+              turbo_transformers::core::NewDLPackTensorT<float>(
+                  {3, batch_size, num_attention_heads, seq_length, hidden_size},
+                  kDLCPU, 0));
 
-        SplitAddBiasTransposeForScore(&output_tensor_gpu, input_tensor_gpu,
-                                      bias_tensor_gpu);
-        SplitAddBiasTransposeForScore(&output_tensor_cpu, input_tensor_cpu,
-                                      bias_tensor_cpu);
-        REQUIRE(common::CheckResultOfCPUAndGPU<float>(output_tensor_cpu,
-                                                      output_tensor_gpu));
-      }
+          SplitAddBiasTransposeForScore(&output_tensor_gpu, input_tensor_gpu,
+                                        bias_tensor_gpu);
+          SplitAddBiasTransposeForScore(&output_tensor_cpu, input_tensor_cpu,
+                                        bias_tensor_cpu);
+          REQUIRE(common::CheckResultOfCPUAndGPU<float>(output_tensor_cpu,
+                                                        output_tensor_gpu));
+        }
 }
 
 TEST_CASE("transpose-gpu-test") {
-  const std::vector<int64_t> num_attention_heads_list{12};
-  const std::vector<int64_t> batch_size_list{1, 20, 24};
-  const std::vector<int64_t> seq_length_list{10, 20, 32, 64, 128};
+  const std::vector<int64_t> num_attention_heads_list{12, 20, 24};
+  const std::vector<int64_t> batch_size_list{
+      1,
+      20,
+  };
+  const std::vector<int64_t> seq_length_list{10, 32, 128};
 
   for (auto num_attention_heads : num_attention_heads_list)
     for (auto batch_size : batch_size_list)
