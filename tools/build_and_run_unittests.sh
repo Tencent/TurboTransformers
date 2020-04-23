b5bec49d611a81ea842deb21e546146dc717bd36
jiaruifang <jiaruifang@tencent.com>
2020-04-23 10:45:19 +0800

    add gpu compilation checking

--- tools/build_and_run_unittests.sh
+++ tools/build_and_run_unittests.sh
@@ -13,15 +13,23 @@
 # See the AUTHORS file for names of contributors.
 
 set -xe
-if [ $# != 2 ] ; then
+if [ $# -lt 2 ] ; then
   echo "USAGE: $0 PATH_TO_CODE WITH_GPU_OR_NOT"
   echo " e.g.: $0 `PWD` -DWITH_GPU=OFF"
   exit 1;
+fi
+
+if [ $# == 3 ] ; then
+  USE_TEST="False"
+else
+  USE_TEST="True"
 fi
 SRC_ROOT=$1
 WITH_GPU=$2
 BUILD_PATH=/tmp/build_cpu
 bash ${SRC_ROOT}/tools/compile.sh ${SRC_ROOT} ${WITH_GPU} ${BUILD_PATH}
 python3 -m pip install -r ${SRC_ROOT}/requirements.txt
-cd ${BUILD_PATH}
-ctest --output-on-failure
+if [ ${USE_TEST} == "True" ]; then
+  cd ${BUILD_PATH}
+  ctest --output-on-failure
+fi
