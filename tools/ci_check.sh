96a06dd46c12d634ffc006ec671f4f6e31fbbd04
jiaruifang <jiaruifang@tencent.com>
2020-04-23 12:52:38 +0800

    conbine CI scripts in a file

--- tools/ci_check.sh
+++ tools/ci_check.sh
@@ -13,16 +13,13 @@
 # See the AUTHORS file for names of contributors.
 
 set -xe
-if [ $# -lt 2 ] ; then
-  echo "USAGE: $0 PATH_TO_CODE WITH_GPU_OR_NOT"
-  echo " e.g.: $0 `PWD` -DWITH_GPU=OFF"
-  exit 1;
-fi
 
 SRC_ROOT=$1
-WITH_GPU=$2
 BUILD_PATH=/tmp/build_cpu
-bash ${SRC_ROOT}/tools/compile.sh ${SRC_ROOT} ${WITH_GPU} ${BUILD_PATH}
+bash ${SRC_ROOT}/tools/compile.sh ${SRC_ROOT} -DWITH_GPU=OFF ${BUILD_PATH}
 python3 -m pip install -r ${SRC_ROOT}/requirements.txt
 cd ${BUILD_PATH}
 ctest --output-on-failure
+
+BUILD_PATH=/tmp/build_gpu
+bash ${SRC_ROOT}/tools/compile.sh ${SRC_ROOT} -DWITH_GPU=ON $BUILD_PATH
