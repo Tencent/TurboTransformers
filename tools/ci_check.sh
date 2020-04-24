92381abc4f5f7d04abbba9954498cba01945492c
jiaruifang <jiaruifang@tencent.com>
2020-04-24 19:09:19 +0800

    update ci

--- tools/ci_check.sh
+++ tools/ci_check.sh
@@ -21,11 +21,11 @@ python3 -m pip install -r ${SRC_ROOT}/requirements.txt
 cd ${BUILD_PATH}
 ctest --output-on-failure
 # test npz model loader
-python ./convert_huggingface_bert_pytorch_to_npz.py bert-base-uncased bert_torch.npz
-python ../example/python/cpu_example.py bert_torch.npz
+python ${SRC_ROOT}/ttols/convert_huggingface_bert_pytorch_to_npz.py bert-base-uncased bert_torch.npz
+python ${SRC_ROOT}/tools/example/python/cpu_example.py bert_torch.npz
 rm bert_torch.npz
-python ./convert_huggingface_bert_tf_to_npz.py bert-base-uncased bert_tf.npz
-python ../example/python/cpu_example.py bert_tf.npz
+python ${SRC_ROOT}/tools/convert_huggingface_bert_tf_to_npz.py bert-base-uncased bert_tf.npz
+python ${SRC_ROOT}/example/python/cpu_example.py bert_tf.npz
 rm bert_tf.npz
 
 BUILD_PATH=/tmp/build_gpu
