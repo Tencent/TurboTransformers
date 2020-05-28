c20fe74d0c7c283d2927997a4d7b21442534adef
jiaruifang <jiaruifang@tencent.com>
2020-05-28 10:01:43 +0800

    init an amd blis support. WIP

--- turbo_transformers/core/blas_blis.cpp
+++ turbo_transformers/core/blas_blis.cpp
@@ -11,21 +11,6 @@
 // permissions and limitations under the License.
 // See the AUTHORS file for names of contributors.
 
-#pragma once
-
-#if defined(TT_BLAS_USE_MKL)
-#include "mkl.h"
-
-namespace turbo_transformers {
-using BlasInt = MKL_INT;
-}
-
-#elif defined(TT_BLAS_USE_OPENBLAS)
-#include "cblas.h"
-namespace turbo_transformers {
-using BlasInt = blasint;
-}  // namespace turbo_transformers
-
 extern "C" {
 void cblas_sgemm_batch(const CBLAS_ORDER Layout,
                        const CBLAS_TRANSPOSE* transa_array,
@@ -36,24 +21,13 @@ void cblas_sgemm_batch(const CBLAS_ORDER Layout,
                        const float** b_array, const blasint* ldb_array,
                        const float* beta_array, float** c_array,
                        const blasint* ldc_array, const blasint group_count,
-                       const blasint* group_size);
-void vsTanh(blasint N, const float* in, float* out);
+                       const blasint* group_size) {
+  // TODO
 }
-#elif defined(TT_BLAS_USE_BLIS)
-#include "blis.h"
-namespace turbo_transformers {
-using BlasInt = dim_t;
-}  // namespace turbo_transformers
-extern "C" {
-void cblas_sgemm_batch(const CBLAS_ORDER Layout,
-                       const CBLAS_TRANSPOSE* transa_array,
-                       const CBLAS_TRANSPOSE* transb_array,
-                       const blasint* m_array, const blasint* n_array,
-                       const blasint* k_array, const float* alpha_array,
-                       const float** a_array, const blasint* lda_array,
-                       const float** b_array, const blasint* ldb_array,
-                       const float* beta_array, float** c_array,
-                       const blasint* ldc_array, const blasint group_count,
-                       const blasint* group_size);
+
+void cblas_sgemm(const CBLAS_ORDER Layout, const CBLAS_TRANSPOSE* transa_array,
+                 const CBLAS_TRANSPOSE* transb_array, const blasint M,
+                 const blasint N, const blasint K_a, float alpha, float* A,
+                 const blasint lda, float* B, const blasint ldb, float beta,
+                 float* C, const blasint ldc) {}
 }
-#endif
