d29645ca31e47f6275aaf1f8070a4144fb52f1a9
feifeibear <jiaruifang@tencent.com>
2020-06-05 14:00:02 +0800

    softmax supports 3D mask (#72)
    gpu softmax support 3D mask.

--- turbo_transformers/layers/kernels/gpu_softmax_kernel.h
+++ turbo_transformers/layers/kernels/gpu_softmax_kernel.h
@@ -20,7 +20,7 @@ namespace kernels {
 template <typename T>
 void GPUSoftmaxMask(T* qk_buf, const T* attr_mask, int64_t batch_size,
                     int64_t head_num, int64_t from_seq_len, int64_t to_seq_len,
-                    float scale, cudaStream_t stream);
+                    float scale, bool is_2D, cudaStream_t stream);
 }  // namespace kernels
 }  // namespace layers
 }  // namespace turbo_transformers
