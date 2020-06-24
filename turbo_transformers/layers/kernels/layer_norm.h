7500f8c476c9a42ee5843abaadaf99572248be67
jiaruifang <jiaruifang@tencent.com>
2020-06-24 10:45:02 +0800

    move profiler into functions.

--- turbo_transformers/layers/kernels/layer_norm.h
+++ turbo_transformers/layers/kernels/layer_norm.h
@@ -22,14 +22,16 @@ namespace kernels {
 
 template <typename T>
 extern void LayerNorm(const core::Tensor& gamma, const core::Tensor& beta,
-                      core::Tensor* out_tensor, T eps = 1e-12);
+                      core::Tensor* out_tensor, T eps = 1e-12,
+                      const std::string name = "LayerNorm");
 
 template <typename T>
 extern void AddBiasLayerNorm(const core::Tensor& input_tensor,
                              const core::Tensor& bias_tensor,
                              const core::Tensor& gamma_tensor,
                              const core::Tensor& beta_tensor,
-                             core::Tensor* out_tensor, T eps = 1e-12);
+                             core::Tensor* out_tensor, T eps = 1e-12,
+                             const std::string name = "AddBiasLayerNorm");
 
 }  // namespace kernels
 }  // namespace layers
