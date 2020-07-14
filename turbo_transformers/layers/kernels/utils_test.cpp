e09bc77c9cc730f115f75ad86a3095eecaa209de
xcnick <xcnick0412@gmail.com>
2020-07-14 09:43:46 +0800

    add unittest for AddBias and fix some typos (#124)

--- turbo_transformers/layers/kernels/utils_test.cpp
+++ turbo_transformers/layers/kernels/utils_test.cpp
@@ -11,7 +11,10 @@
 // permissions and limitations under the License.
 // See the AUTHORS file for names of contributors.
 #include "turbo_transformers/layers/kernels/utils.h"
-
+#ifdef TT_WITH_CUDA
+#include "turbo_transformers/core/cuda_device_context.h"
+#include "turbo_transformers/layers/kernels/common.h"
+#endif
 #include "catch2/catch.hpp"
 
 namespace turbo_transformers {
@@ -32,13 +35,16 @@ TEST_CASE("cpu-concate", "test1") {
   }
   turbo_transformers::core::Tensor res1(nullptr), res2(nullptr), res3(nullptr);
   Concat<float>(t1, t2, 2, &res1);
-  // res1.Print<float>(std::cerr);
+  REQUIRE(res1.n_dim() == 4);
+  REQUIRE(res1.numel() == 2 * 1 * 5 * 2);
 
   turbo_transformers::core::Tensor t3(
       turbo_transformers::core::NewDLPackTensorT<float>({4, 2}));
   turbo_transformers::core::Tensor t4(
       turbo_transformers::core::NewDLPackTensorT<float>({3, 2}));
   Concat<float>(t3, t4, 0, &res2);
+  REQUIRE(res2.n_dim() == 2);
+  REQUIRE(res2.numel() == 7 * 2);
 
   turbo_transformers::core::Tensor t5(
       turbo_transformers::core::NewDLPackTensorT<float>({2, 3}));
@@ -51,8 +57,9 @@ TEST_CASE("cpu-concate", "test1") {
   for (int i = 0; i < t6.numel(); ++i) {
     t6.mutableData<float>()[i] = i * 100.0;
   }
-  Concat<float>(t5, t6, 1, &res2);
-  // res3.Print<float>(std::cerr);
+  Concat<float>(t5, t6, 1, &res3);
+  REQUIRE(res3.n_dim() == 2);
+  REQUIRE(res3.numel() == 2 * 7);
 
   // t5.Print<float>(std::cerr);
   // t6.Print<float>(std::cerr);
@@ -60,6 +67,72 @@ TEST_CASE("cpu-concate", "test1") {
   // REQUIRE(test_tensor.n_dim() == 2);
   // REQUIRE(test_tensor.numel() == 3 * 4);
 }
+
+#ifdef TT_WITH_CUDA
+template <typename T, typename Func>
+static void AddBiasTestHelper(int batch_size, int seq_length,
+                              int hidden_size, const Func& func) {
+  core::Tensor gpu_bias(nullptr), cpu_bias(nullptr), gpu_out(nullptr),
+      cpu_out(nullptr);
+  std::tie(cpu_bias, gpu_bias) =
+      common::CreateAndFillRandomForCPUGPUTensors<T>({hidden_size});
+  std::tie(cpu_out, gpu_out) = common::CreateAndFillRandomForCPUGPUTensors<T>(
+      {batch_size, seq_length, hidden_size});
+  func(cpu_bias, cpu_out, gpu_bias, gpu_out);
+}
+
+TEST_CASE("addbias-gpu-test") {
+  for (auto hidden_size : {512, 12 * 64, 4096 * 2 + 1}) {
+    for (auto batch_size : {1, 5}) {
+      for (auto seq_length : {8, 120}) {
+        AddBiasTestHelper<float>(
+            batch_size, seq_length, hidden_size,
+            [](core::Tensor& cpu_bias, core::Tensor& cpu_out,
+               core::Tensor& gpu_bias, core::Tensor& gpu_out) {
+              AddBias(cpu_bias, &cpu_out);
+              AddBias(gpu_bias, &gpu_out);
+              REQUIRE(common::CheckResultOfCPUAndGPU<float>(cpu_out, gpu_out));
+            });
+      }  // for
+    }
+  }
+}
+
+template <typename T, typename Func>
+static void AddInputBiasTestHelper(int batch_size, int seq_length,
+                                   int hidden_size, const Func& func) {
+  core::Tensor gpu_input1(nullptr), cpu_input1(nullptr), gpu_input2(nullptr), cpu_input2(nullptr),
+               gpu_bias(nullptr), cpu_bias(nullptr), gpu_out(nullptr), cpu_out(nullptr);
+  std::tie(cpu_input1, gpu_input1) = common::CreateAndFillRandomForCPUGPUTensors<T>(
+      {batch_size, seq_length, hidden_size});
+  std::tie(cpu_input2, gpu_input2) = common::CreateAndFillRandomForCPUGPUTensors<T>(
+      {batch_size, seq_length, hidden_size});
+  std::tie(cpu_bias, gpu_bias) =
+      common::CreateAndFillRandomForCPUGPUTensors<T>({hidden_size});
+  std::tie(cpu_out, gpu_out) = common::CreateAndFillRandomForCPUGPUTensors<T>(
+      {batch_size, seq_length, hidden_size});
+  func(cpu_input1, cpu_input2, cpu_bias, cpu_out, gpu_input1, gpu_input2, gpu_bias, gpu_out);
+}
+
+TEST_CASE("addinputbias-gpu-test") {
+  for (auto hidden_size : {512, 12 * 64, 4096 * 2 + 1}) {
+    for (auto batch_size : {1, 5}) {
+      for (auto seq_length : {8, 120}) {
+        AddInputBiasTestHelper<float>(
+            batch_size, seq_length, hidden_size,
+            [](core::Tensor& cpu_input1, core::Tensor& cpu_input2,
+               core::Tensor& cpu_bias, core::Tensor& cpu_out,
+               core::Tensor& gpu_input1, core::Tensor& gpu_input2,
+               core::Tensor& gpu_bias, core::Tensor& gpu_out) {
+              AddInputBias(cpu_input1, cpu_input2, cpu_bias, &cpu_out);
+              AddInputBias(gpu_input1, gpu_input2, gpu_bias, &gpu_out);
+              REQUIRE(common::CheckResultOfCPUAndGPU<float>(cpu_out, gpu_out));
+            });
+      }  // for
+    }
+  }
+}
+#endif
 
 }  // namespace kernels
 }  // namespace layers
