738b9b630a18a09a9a4dcf76dcf627454d00fe72
jiaruifang <fangjiarui123@gmail.com>
2020-11-25 11:04:07 +0800

    albert model uses model-aware allocator.

--- turbo_transformers/python/tests/albert_model_test.py
+++ turbo_transformers/python/tests/albert_model_test.py
@@ -17,89 +17,106 @@ import sys
 import torch
 import turbo_transformers
 from transformers.modeling_albert import AlbertConfig, AlbertModel
-import numpy
 import os
 
 sys.path.append(os.path.dirname(__file__))
 import test_helper
 
 
-def create_test(batch_size, seq_length):
-    class TestAlbertModel(unittest.TestCase):
-        def init_data(self, use_cuda: bool) -> None:
-            self.test_device = torch.device('cuda:0') if use_cuda else \
-                torch.device('cpu:0')
-            if not use_cuda:
-                torch.set_num_threads(4)
-                turbo_transformers.set_num_threads(4)
+class TestAlbertModel(unittest.TestCase):
+    def init_data(self, use_cuda: bool) -> None:
+        self.test_device = torch.device('cuda:0') if use_cuda else \
+            torch.device('cpu:0')
+        if not use_cuda:
+            torch.set_num_threads(4)
+            turbo_transformers.set_num_threads(4)
 
-            torch.set_grad_enabled(False)
-            self.cfg = AlbertConfig()
+        torch.set_grad_enabled(False)
+        self.cfg = AlbertConfig(hidden_size=768,
+                                num_attention_heads=12,
+                                intermediate_size=3072)
+        self.torch_model = AlbertModel(self.cfg)
 
-            self.torch_model = AlbertModel(self.cfg)
-            if torch.cuda.is_available():
-                self.torch_model.to(self.test_device)
-            self.torch_model.eval()
-            self.hidden_size = self.cfg.hidden_size
-            self.input_tensor = torch.randint(low=0,
-                                              high=self.cfg.vocab_size - 1,
-                                              size=(batch_size, seq_length),
-                                              device=self.test_device)
+        if torch.cuda.is_available():
+            self.torch_model.to(self.test_device)
+        self.torch_model.eval()
+        self.hidden_size = self.cfg.hidden_size
 
-            self.turbo_model = turbo_transformers.AlbertModel.from_torch(
-                self.torch_model)
+        self.turbo_model = turbo_transformers.AlbertModel.from_torch(
+            self.torch_model)
 
-        def check_torch_and_turbo(self, use_cuda):
-            self.init_data(use_cuda=use_cuda)
-            device = "GPU" if use_cuda else "CPU"
-            num_iter = 1
-            turbo_model = lambda: self.turbo_model(
-                self.input_tensor, attention_mask=None, head_mask=None)
-            turbo_result, turbo_qps, turbo_time = \
-                test_helper.run_model(turbo_model, use_cuda, num_iter)
+    def check_torch_and_turbo(self, batch_size, seq_length, use_cuda,
+                              use_memory_opt):
+        self.init_data(use_cuda=use_cuda)
+        self.input_tensor = torch.randint(low=0,
+                                          high=self.cfg.vocab_size - 1,
+                                          size=(batch_size, seq_length),
+                                          device=self.test_device)
 
-            print(
-                f"AlbertLayer \"({batch_size},{seq_length:03})\" ",
-                f"{device} TurboTransform QPS,  {turbo_qps}, time, {turbo_time}"
+        device = "GPU" if use_cuda else "CPU"
+        num_iter = 1
+
+        if use_memory_opt:
+            turbo_transformers.bert_opt_mem_allocate_api(
+                self.input_tensor.size()[0],  # batch
+                self.input_tensor.size()[1],  # seq_len
+                self.cfg.num_attention_heads,
+                self.cfg.hidden_size,
+                self.cfg.num_hidden_layers,
+                "GPU" if 'cuda' in self.input_tensor.device.type else "CPU")
+
+        turbo_model = lambda: self.turbo_model(
+            self.input_tensor, attention_mask=None, head_mask=None)
+        turbo_result, turbo_qps, turbo_time = \
+            test_helper.run_model(turbo_model, use_cuda, num_iter)
+
+        print(
+            f"AlbertLayer \"({batch_size},{seq_length:03})\" ",
+            f"{device} TurboTransform QPS,  {turbo_qps}, time, {turbo_time}")
+        torch_model = lambda: self.torch_model(
+            input_ids=self.input_tensor, attention_mask=None, head_mask=None)
+        with turbo_transformers.pref_guard("albert_perf") as perf:
+            torch_result, torch_qps, torch_time = \
+                test_helper.run_model(torch_model, use_cuda, num_iter)
+
+        print(f"AlbertModel \"({batch_size},{seq_length:03})\" ",
+              f"{device} Torch QPS,  {torch_qps}, time, {torch_time}")
+
+        # print(turbo_result[-1])
+        # print(turbo_result, torch_result[0])
+        # TODO(jiaruifang) Error is too high. Does tensor core introduce more differences?
+        tolerate_error = 1e-2
+        self.assertTrue(
+            torch.max(torch.abs(torch_result[0] -
+                                turbo_result[0])) < tolerate_error)
+
+        with open("albert_model_res.txt", "a") as fh:
+            fh.write(
+                f"\"({batch_size},{seq_length:03})\", {torch_qps}, {torch_qps}\n"
             )
-            torch_model = lambda: self.torch_model(input_ids=self.input_tensor,
-                                                   attention_mask=None,
-                                                   head_mask=None)
-            with turbo_transformers.pref_guard("albert_perf") as perf:
-                torch_result, torch_qps, torch_time = \
-                    test_helper.run_model(torch_model, use_cuda, num_iter)
 
-            print(f"AlbertModel \"({batch_size},{seq_length:03})\" ",
-                  f"{device} Torch QPS,  {torch_qps}, time, {torch_time}")
+    def albert_model_test_helper(self, use_memory_opt):
+        if use_memory_opt:
+            turbo_transformers.reset_allocator_schema("model-aware")
+            for batch_size in [1, 2]:
+                for seq_length in [50, 10, 64]:
+                    self.check_torch_and_turbo(batch_size,
+                                               seq_length,
+                                               use_cuda=False,
+                                               use_memory_opt=True)
+                    if torch.cuda.is_available() and \
+                        turbo_transformers.config.is_compiled_with_cuda():
+                        self.check_torch_and_turbo(batch_size,
+                                                   seq_length,
+                                                   use_cuda=True,
+                                                   use_memory_opt=True)
+        if use_memory_opt:
+            turbo_transformers.reset_allocator_schema("naive")
 
-            # print(turbo_result[-1])
-            # print(turbo_result, torch_result[0])
-            # TODO(jiaruifang) Error is too high. Does tensor core introduce more differences?
-            tolerate_error = 1e-2
-            self.assertTrue(
-                torch.max(torch.abs(torch_result[0] -
-                                    turbo_result[0])) < tolerate_error)
+    def test(self):
+        albert_model_test_helper(False)
+        albert_model_test_helper(True)
 
-            with open("albert_model_res.txt", "a") as fh:
-                fh.write(
-                    f"\"({batch_size},{seq_length:03})\", {torch_qps}, {torch_qps}\n"
-                )
-
-        def test_layer(self):
-            self.check_torch_and_turbo(use_cuda=False)
-            if torch.cuda.is_available() and \
-                turbo_transformers.config.is_compiled_with_cuda():
-                self.check_torch_and_turbo(use_cuda=True)
-
-    globals()[f"TestAlbertModel{batch_size}_{seq_length:03}"] = \
-        TestAlbertModel
-
-
-with open("albert_model_res.txt", "w") as fh:
-    fh.write(", torch, turbo_transformers\n")
-for batch_size in [1, 2]:
-    for seq_length in [10]:
-        create_test(batch_size, seq_length)
 
 if __name__ == '__main__':
     unittest.main()
